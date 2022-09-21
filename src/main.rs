f0ec64554ae183584445f4c606b5eac92089a8c3
Uian Sol <usoldeveloper@gmail.com>
2022-09-21 17:29:25 -0300

    Extract logic to lib

--- src/main.rs
+++ src/main.rs
@@ -14,30 +14,14 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
-        }
-    }
-}
-
 fn main() -> Result<()> {
     let args: Cli = Cli::parse();
-    // let content: String = std::fs::read_to_string(&args.path)?;
 
     let path = "test.txt";
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
 
     Ok(())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
-    assert_eq!(result, b"lorem ipsum\n")
 }
